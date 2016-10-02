27c3abe9a042418d3acd30b8471c3165bfcb9da6
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2016-10-02 14:35:00 -0400

    Add -n/--dry-run option

--- cleanup.py
+++ cleanup.py
@@ -5,7 +5,9 @@ from gitlab import Gitlab
 from gitlab.exceptions import *
 
 class GitlabArtifactCleanup(object):
-    def __init__(self):
+    def __init__(self, dry_run=False):
+        self.dry_run = dry_run
+
         self.total_deleted = 0
 
 
@@ -26,9 +28,14 @@ class GitlabArtifactCleanup(object):
 
             af = build.artifacts_file
             total_size += af['size']
-            print('  Build {}: Deleting {} ({} bytes)'.format(build.id, af['filename'], af['size']))
+            print('  Build {id}: {action} {filename} ({size} bytes)'.format(
+                id = build.id,
+                action = 'Would delete' if self.dry_run else 'Deleting',
+                filename = af['filename'],
+                size = af['size']))
 
-            build.erase()
+            if not self.dry_run:
+                build.erase()
 
         self.total_deleted += total_size
         return total_size
@@ -40,6 +47,8 @@ def parse_args():
             help='GitLab server defined in config file .python-gitlab')
     ap.add_argument('--all-projects', action='store_true',
             help='Cleanup artifacts for all accessible projects')
+    ap.add_argument('-n', '--dry-run', action='store_true',
+            help="Don't actually delete anything")
     ap.add_argument('-p', '--project', dest='projects', action='append',
             help='Project for which to cleanup artifacts -- can be given multiple times')
 
@@ -56,7 +65,9 @@ def main():
     args = parse_args()
     gl = Gitlab.from_config(gitlab_id=args.gitlab)
 
-    cleanup = GitlabArtifactCleanup()
+    cleanup = GitlabArtifactCleanup(
+            dry_run = args.dry_run,
+            )
 
     if args.all_projects:
         for proj in gl.projects.all(all=True):
@@ -69,9 +80,13 @@ def main():
                 print("Error getting project", pname, e)
                 continue
             deleted = cleanup.cleanup_project(proj)
-            print('  Deleted {} bytes of artifacts'.format(deleted))
+            print('  {action} {size} bytes of artifacts'.format(
+                action = 'Would delete' if args.dry_run else 'Deleted',
+                size = deleted))
 
-    print('Deleted {} bytes of artifacts total'.format(cleanup.total_deleted))
+    print('{action} {size} bytes of artifacts total'.format(
+        action = 'Would delete' if args.dry_run else 'Deleted',
+        size = cleanup.total_deleted))
 
 if __name__ == '__main__':
     main()
